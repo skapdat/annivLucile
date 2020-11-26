9b940f914e02aa219ba0cd534ac4fcd5b529e6a6
Skapdat <skapdaht@gmail.com>
2020-11-26 09:42:20 +0100

    [base] app

--- src/App.js
+++ src/App.js
@@ -1,17 +1,7 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
   return (
     <main>
       <p>Test</p>
