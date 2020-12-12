678f41cbaae6bea3f631a13c9f14fa46fb2cfbfe
Skapdat <skapdaht@gmail.com>
2020-12-12 18:49:53 +0100

    [base] base fonctionnelle

--- src/Admin.js
+++ src/Admin.js
@@ -1,7 +1,21 @@
 
 import React, { Component } from 'react';
+import firebase from "firebase/app";
+import "firebase/database";
 
 class Home extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {quest: []};
+      const ref = firebase.database().ref('videos');
+      ref.once('value', (snapshot) => {
+        var dataQuest = [];
+        snapshot.forEach(childSnapshot => {
+            dataQuest[childSnapshot.key] = childSnapshot.val();
+        });
+        this.setState({quest: dataQuest});
+      });
+  }
   render() {
     return (
         <main>Admin</main>
--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,52 @@
 import React, { Component } from 'react';
 import firebase from "firebase/app";
 import "firebase/database";
-
+import Vimeo from '@u-wave/react-vimeo';
 class Home extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {quest: []};
-        const ref = firebase.database().ref('videos');
-        ref.once('value', (snapshot) => {
+  constructor(props) {
+      super(props);
+      this.state = {quest: [], actVid: null};
+      const ref = firebase.database().ref('videos');
+      ref.once('value', (snapshot) => {
         var dataQuest = [];
         snapshot.forEach(childSnapshot => {
             dataQuest[childSnapshot.key] = childSnapshot.val();
         });
         this.setState({quest: dataQuest});
-        });
-    }
+      });
+  }
+
+  onVidClicked(idVid) {
+    const {quest} = this.state;
+    var nQuest = Object.assign({}, quest);
+    nQuest[idVid].seen = true;
+    firebase.database().ref('videos').set(nQuest);
+    this.setState({
+      actVid: quest[idVid],
+      quest: nQuest
+    });
+  }
+
   render() {
     return (
         <main>
           {
-            this.state.quest.map((e, i) =>  <p key={i} data-link={e}>{i}</p>)
+            Object.values(this.state.quest).map((vid, i) => 
+              <p key={i} onClick={() => this.onVidClicked(i)}>
+              {vid.seen ? "vu" : i}
+              </p>
+            )
+          }
+          {
+            this.state.actVid && (
+            <div id='playCt'>
+              <button onClick={() =>this.setState({actVid: null})}>x</button>
+              <Vimeo
+                video={this.state.actVid.vid}
+                autoplay
+              />
+            </div>
+            )
           }
         </main>
     );
